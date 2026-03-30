e783db6385349b2b1531e8ba3958b405731ca7d7
User <user@example.com>
2026-03-30 14:38:06 +0000

    Some corrections

--- config/env.py
+++
@@ -1,6 +0,0 @@
-# config/env.py
-from pathlib import Path
-
-# TODO change this to .env.prod in production env
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
-CUR_ENV_FILE = BASE_DIR / ".envs" / ".env.local"
