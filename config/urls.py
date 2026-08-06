ad2605d507832c90a35d967a4fad92f761b35674
@yiek <ayiekue@outlook.com>
2026-08-06 18:05:21 +0000

    chore: rebrand Sycosur to Koda in API docs and admin site - Update Swagger API title and contact email - Update Django admin site header, title, and index title

--- config/urls.py
+++ config/urls.py
@@ -9,10 +9,10 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title=" SYCOSUR API",
+        title=" Koda API",
         default_version="v1",
         description="Data management API",
-        contact=openapi.Contact(email="ekue.ayi@insuco.com"),
+        contact=openapi.Contact(email="ekue.ayi@kodaap.com"),
         license=openapi.License(name="MIT License"),
     ),
     public=True,
@@ -34,9 +34,9 @@ urlpatterns = [
     path("api/v1/odk/", include("core_apps.odk.urls")),
 ]
 
-admin.site.site_header = "Sycosur2.0"
-admin.site.site_title = "Sycosur Admin Portal"
-admin.site.index_title = "Welcome to Sycosur 2.0"
+admin.site.site_header = "Kodaap"
+admin.site.site_title = "Koda Admin Portal"
+admin.site.index_title = "Welcome to Koda Survey App"
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
