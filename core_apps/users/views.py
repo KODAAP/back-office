03d284cd26bb17cb4af71b0cc4a326162c65af1f
@yiek <ayiekue9127@gmail.com>
2026-08-06 18:13:19 +0000

    fix(auth): remove redundant AllowAny permission on CustomProviderAuthView

--- core_apps/users/views.py
+++ core_apps/users/views.py
@@ -132,7 +132,6 @@ class CustomTokenRefreshView(TokenRefreshView):
 
 
 class CustomProviderAuthView(ProviderAuthView):
-    permission_classes = AllowAny
 
     def post(self, request: Request, *args, **kwargs) -> Response:
         provider_res = super().post(request, *args, **kwargs)
